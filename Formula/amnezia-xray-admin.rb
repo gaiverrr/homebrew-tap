7a09a9577df180fadd5a710249cfe7af3bb98e8f
Yuriy Kaygorodov <gaiverrr@gmail.com>
2026-03-21 20:35:08 +0000

    Add amnezia-xray-admin formula for v0.1.0

---
+++ Formula/amnezia-xray-admin.rb
@@ -0,0 +1,17 @@
+class AmneziaXrayAdmin < Formula
+  desc "TUI dashboard for managing Amnezia VPN's Xray server"
+  homepage "https://github.com/gaiverrr/amnezia-xray-admin"
+  url "https://github.com/gaiverrr/amnezia-xray-admin/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "PLACEHOLDER"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match "amnezia-xray-admin", shell_output("#{bin}/amnezia-xray-admin --help")
+  end
+end
