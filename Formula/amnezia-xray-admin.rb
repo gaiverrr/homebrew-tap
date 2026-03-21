391467f2e8e537710850292775ea86ecfa9b420b
Yuriy Kaygorodov <gaiverrr@gmail.com>
2026-03-21 21:05:18 +0000

    Update formula SHA256 for v0.1.0 release

--- Formula/amnezia-xray-admin.rb
+++ Formula/amnezia-xray-admin.rb
@@ -2,7 +2,7 @@ class AmneziaXrayAdmin < Formula
   desc "TUI dashboard for managing Amnezia VPN's Xray server"
   homepage "https://github.com/gaiverrr/amnezia-xray-admin"
   url "https://github.com/gaiverrr/amnezia-xray-admin/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "PLACEHOLDER"
+  sha256 "0c87abd287c935e877ae0fbf36eb3d1628e6d9f1c11bf2885d1c016a25f6904f"
   license "MIT"
 
   depends_on "rust" => :build
